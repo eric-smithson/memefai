c14113be039a2e82b9aabf423ba6f3f6d09acd84
Eric Smithson <ericsmithson@Erics-MacBook-Pro.local>
2017-01-21 13:03:16 -0500

    wip, cleaned up reddit handler skeleton

--- reddit-bot/reddit_handler.py
+++ reddit-bot/reddit_handler.py
@@ -1,16 +1,16 @@
 # this where we will build the functions which handle all our reddit interactions
+import praw
 
-def get_post_url():
-    import praw
+def get_image_post_url():
     reddit = praw.Reddit(user_agent='MemefaiBot for SwampHacks', client_id='2UscbBFgmqdBMA',
                          client_secret='yY3dSQTQy2tBWTQVi3eW1vuyf-I', username='IAmJimmyNeutron', password='67y890')
     subreddit = reddit.subreddit("me_irl")
+    submissions_list = []
     for submission in subreddit.hot(limit=5):
         if submission.score > 500:
             print submission.url
-
-def get_post_image_url():
-    pass
+            submissions_list.append(submission.url)
+    return submissions_list
 
 def make_comment():
     pass
