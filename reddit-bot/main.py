1a51a3fa1362d3bad173d37230a8c4682c81447f
Eric Smithson <ericsmithson@Erics-MacBook-Pro.local>
2017-01-21 14:21:37 -0500

    added skeleton for how OCR handling will take place, wip

--- reddit-bot/main.py
+++ reddit-bot/main.py
@@ -4,26 +4,34 @@ from reddit_handler import *
 # this is where we do all the main action, we will make call to the other files from this script
 
 # fill out with our message template, will have to be formatted in reddit markup
-MESSAGE_TEMPLATE = '''Meme has these tags:
-    $__TAGS__$
-'''
+MESSAGE_TEMPLATE = '''Memefai_Bot has detected these tags:
 
+    $__TAGS__$$__TEXT_BLURB__$
+
+^(Like this bot? Contribute at github.com/eric-smithson/memefai)
+'''
 # get url from reddit
 # submission_url = get_submission_url()
 
 # get image url from reddit
 # image_url = get_image_post_url()
 image_url_list = get_image_post_url()
-image_url_list.start()
-image_url_list.join()
 
 # send image to clarifai
 for image_url in image_url_list:
     image_tags = get_tags(image_url) # this function is defined in clarifai_handler.py
 
     # format the message template to include the tags
-    message = MESSAGE_TEMPLATE.replace(("$__TAGS__$"), (', ').join(image_tags))
+    message = MESSAGE_TEMPLATE.replace("$__TAGS__$", (', ').join(image_tags))
+    # TODO: the code below is placeholder, needs to be updated with actual OCR calls
+    has_text = True
+    if(has_text):
+        message = message.replace("$__TEXT_BLURB__$", "\n\nTranscribed text in meme: \n\n    $__TEXT__$.")
+        message = message.replace("$__TEXT__$", "I LIKE DANK MEMES")
+    else:
+        message = message.replace("$__TEXT_BLURB__$", "")
     print message
+    make_comment(message)
 
 # send comment to reddit to be posted
 # make_comment(message)
--- reddit-bot/reddit_handler.py
+++ reddit-bot/reddit_handler.py
@@ -8,12 +8,12 @@ def get_image_post_url():
     submissions_list = []
     for submission in subreddit.hot(limit=5):
         if submission.score > 500:
-            print submission.url
             url = submission.url
             if url[7:12] == 'imgur':
                 url = 'http://i.' + url[7:] + '.jpg'
+            print url
             submissions_list.append(url)
     return submissions_list
 
-def make_comment():
+def make_comment(body):
     pass
