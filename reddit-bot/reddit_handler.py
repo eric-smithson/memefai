d05d5b39427b7d6bb0d188da0fcd8e1c7f8a5879
Brandon Hahn <blh16b@my.fsu.edu>
2017-01-21 11:34:54 -0500

    fleshed out get_post_url function in reddit_handler

--- reddit-bot/reddit_handler.py
+++ reddit-bot/reddit_handler.py
@@ -1,7 +1,13 @@
 # this where we will build the functions which handle all our reddit interactions
 
 def get_post_url():
-    pass
+    import praw
+    reddit = praw.Reddit(user_agent='MemefaiBot for SwampHacks', client_id='2UscbBFgmqdBMA',
+                         client_secret='yY3dSQTQy2tBWTQVi3eW1vuyf-I', username='IAmJimmyNeutron', password='67y890')
+    subreddit = reddit.subreddit("me_irl")
+    for submission in subreddit.hot(limit=5):
+        if submission.score > 500:
+            print submission.url
 
 def get_post_image_url():
     pass
