1536dc4577dc78e497e4f651caaffde667920829
Eric Smithson <ericsmithson@Erics-MacBook-Pro.local>
2017-01-21 16:38:08 -0500

    Now logs stuff in the database!

---
+++ reddit-bot/json_handler.py
@@ -0,0 +1,34 @@
+import json
+import os
+# handles all json interactions
+
+#todo: make sure when this script is run that it creates the neccesary filepaths so it doesn't crash looking for files that aren't there when it runs the first time
+
+def check_if_url_in_db(url):
+    with open(log_dir + "/database.json", "r") as log_fp:
+        url_dict = json.load(log_fp)
+        return url in url_dict
+
+
+log_dir = os.getcwd() + '/data/logs'
+
+def put_in_db(url, tags, text):
+    url_dict = {}
+    with open(log_dir + "/database.json", "r") as log_fp:
+        url_dict = json.load(log_fp)
+    log_fp.close()
+
+    temp_dict = {}
+    temp_dict["tags"] = tags
+    temp_dict["text"] = text
+    url_dict[url] = temp_dict
+    with open(log_dir + "/database.json", "w") as log_fp:
+        json.dump(url_dict, log_fp, indent=1)
+    return
+
+dict = {}
+
+dict["hello"] = "greeting"
+dict["swiggity"] = "swoggity"
+
+print dict["hello"]
--- reddit-bot/main.py
+++ reddit-bot/main.py
@@ -1,39 +1,48 @@
 from clarifai_handler import *
 from reddit_handler import *
 from ocr_handler import *
+from json_handler import *
 import json
 
 # this is where we do all the main action, we will make call to the other files from this script
 
 # fill out with our message template, will have to be formatted in reddit markup
-MESSAGE_TEMPLATE = '''Memefai_Bot has detected these tags:
+MESSAGE_TEMPLATE = '''MemefaiBot has detected these tags:
 
     $__TAGS__$$__TEXT_BLURB__$
 
 ^(Like this bot? Contribute at github.com/eric-smithson/memefai)
 '''
-# get url from reddit
-# submission_url = get_submission_url()
 
 # get image url from reddit
-# image_url = get_image_post_url()
 image_url_list = get_image_post_url()
+
+# todo: find a way to get the titles, this is actually important because the titles differ with the spacing between "me" and "irl", usually with emoji.
 
 # send image to clarifai
 for image_url in image_url_list:
+
+    if check_if_url_in_db(image_url): # returns true if the url is already in our db
+        continue
+
     image_tags = get_tags(image_url) # this function is defined in clarifai_handler.py
 
     # format the message template to include the tags
     message = MESSAGE_TEMPLATE.replace("$__TAGS__$", (', ').join(image_tags))
-    # TODO: the code below is placeholder, needs to be updated with actual OCR calls
-    has_text = False
+
+    # TODO: build out OCR calls
+    text_in_image = ""
     if(does_image_have_text(image_url)):
         message = message.replace("$__TEXT_BLURB__$", "\n\nDetected text in meme: \n\n    $__TEXT__$.")
-        message = message.replace("$__TEXT__$", get_text_in_image(image_url))
+        text_in_image = get_text_in_image(image_url)
+        message = message.replace("$__TEXT__$", text_in_image)
     else:
         message = message.replace("$__TEXT_BLURB__$", "")
-    print message
-    make_comment(message)
 
-# send comment to reddit to be posted
-# make_comment(message)
+    print message
+
+    # sends info to be stored in the database
+    put_in_db(image_url, image_tags, text_in_image)
+
+    # sends comment to reddit to be posted
+    make_comment(message)
