beca111990c7ca14dbe214cccfe581285eefd066
Eric Smithson <ericsmithson@Erics-MacBook-Pro.local>
2017-01-21 22:55:37 -0500

    password now saved in external file

--- reddit-bot/reddit_handler.py
+++ reddit-bot/reddit_handler.py
@@ -1,9 +1,10 @@
 # this where we will build the functions which handle all our reddit interactions
 import praw
+import os
 
 def get_image_post_url():
     reddit = praw.Reddit(user_agent='MemefaiBot for SwampHacks', client_id='2UscbBFgmqdBMA',
-                         client_secret='yY3dSQTQy2tBWTQVi3eW1vuyf-I', username='IAmJimmyNeutron', password='')
+                         client_secret='yY3dSQTQy2tBWTQVi3eW1vuyf-I', username='MemefaiBot', password=get_password())
     subreddit = reddit.subreddit("me_irl")
     submissions_dict = {}
     for submission in subreddit.hot(limit=25):
@@ -19,8 +20,12 @@ def get_image_post_url():
 def make_comment(message, submission):
     from urllib import quote_plus
     reddit = praw.Reddit(user_agent='MemefaiBot for SwampHacks', client_id='2UscbBFgmqdBMA',
-                         client_secret='yY3dSQTQy2tBWTQVi3eW1vuyf-I', username='IAmJimmyNeutron', password='')
+                         client_secret='yY3dSQTQy2tBWTQVi3eW1vuyf-I', username='MemefaiBot', password=get_password())
     reply_template = message
     # url_title = quote_plus(submission.title)
     # reply_text = reply_template.format(url_title)
     submission.reply(message)
+
+def get_password():
+    with open(os.getcwd() + "/data/password.txt", "r") as pass_fp:
+        return pass_fp.read()
